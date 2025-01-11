7301bd0a97ad0d4f38bb827e5d2ea9757f28bbf5
uf2121 <umarfarooque241@gmail.com>
2025-01-11 14:44:56 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import "./App.css";
+import Image from "./image.png"; // Importing logo here
+
+
+function App() {
+  const [items, setItems] = useState([]);
+  const [input, setInput] = useState("");
+
+  const handleAddItem = () => {
+    if (input.trim() !== "") {
+      setItems([...items, input.trim()]);
+      setInput("");
+    }
+  };
+
+  return (
+    <div className="app">
+      <header className="header">
+      <img src={Image} alt="Logo" className="logo" /> {/* Adding logo here */}
+        <h1 className="header-title">Item List Manager</h1>
+      </header>
+      <main className="main-container">
+        <div className="input-section">
+          <input
+            type="text"
+            className="input-box"
+            placeholder="Enter item"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />
+          <button className="add-button" onClick={handleAddItem}>
+            Add Item
+          </button>
+        </div>
+        <ul className="item-list">
+          {items.map((item, index) => (
+            <li key={index} className="item">
+              {item}
+            </li>
+          ))}
+        </ul>
+      </main>
+    </div>
+  );
+}
+
+export default App;
